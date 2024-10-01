25f8f8b660d4977cd4b75f3171f199701d9bcb02
Lucas <lucaspgois@gmail.com>
2024-10-01 18:08:27 -0300

    Added frontend stuff
    - Inertia
    - Vue.js
    - TailwindCSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+  title: (title) => `${title} - ${appName}`,
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    return pages[`./Pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue)
+      .mount(el)
+  },
+  progress: {
+      color: '#4B5563',
+  },
+})
